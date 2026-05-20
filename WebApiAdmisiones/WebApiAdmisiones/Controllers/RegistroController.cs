146174d7c15b6cdb74e2d8f70c6be0ee441f9409
Emanuelle Gabriele González <gabriele@ort.edu.uy>
2026-05-20 16:44:44 -0300

    Disable RequireCaptcha on RegistroController POST endpoints
    Comment out [RequireCaptcha] on ConfirmarPersonaExistente, ConfirmarNuevaPersona, and ConfirmarSolicitudAlta actions to temporarily disable CAPTCHA validation. This change is likely for testing or debugging and can be easily reverted by uncommenting the attribute.

--- WebApiAdmisiones/WebApiAdmisiones/Controllers/RegistroController.cs
+++ WebApiAdmisiones/WebApiAdmisiones/Controllers/RegistroController.cs
@@ -113,7 +113,7 @@ namespace WebApiAdmisiones.Controllers
         /// <response code="200">Persona existente confirmada correctamente.</response>
         /// <response code="400">Datos invalidos, captcha invalido o regla funcional no cumplida.</response>
         [AllowAnonymous]
-        [RequireCaptcha]
+        //[RequireCaptcha]
         [HttpPost("ConfirmarPersonaExistente")]
         [ProducesResponseType(typeof(OperationResult<object>), 200)]
         [ProducesResponseType(typeof(OperationResult<object>), 400)]
@@ -134,7 +134,7 @@ namespace WebApiAdmisiones.Controllers
         /// <response code="200">Persona nueva confirmada correctamente.</response>
         /// <response code="400">Datos invalidos, captcha invalido o regla funcional no cumplida.</response>
         [AllowAnonymous]
-        [RequireCaptcha]
+        //[RequireCaptcha]
         [HttpPost("ConfirmarNuevaPersona")]
         [ProducesResponseType(typeof(OperationResult<object>), 200)]
         [ProducesResponseType(typeof(OperationResult<object>), 400)]
@@ -155,7 +155,7 @@ namespace WebApiAdmisiones.Controllers
         /// <response code="200">Solicitud de alta confirmada correctamente.</response>
         /// <response code="400">Datos invalidos, captcha invalido o regla funcional no cumplida.</response>
         [AllowAnonymous]
-        [RequireCaptcha]
+        //[RequireCaptcha]
         [HttpPost("ConfirmarSolicitudAlta")]
         [ProducesResponseType(typeof(OperationResult<object>), 200)]
         [ProducesResponseType(typeof(OperationResult<object>), 400)]
