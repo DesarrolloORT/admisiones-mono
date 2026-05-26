493b36673588fed7468441ab26b06b2a2bdc0a13
rubino-f <rubino_f@ort.edu.uy>
2026-05-26 09:13:54 -0300

    feat(routes): restructure home routes to include HomeLayout and personal data page

--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 
+import { HomeLayout } from './layouts/home-layout/home-layout';
 import { ChangePassword } from './pages/change-password/change-password';
 import { Home } from './pages/home/home';
+import { PersonalData } from './pages/personal-data/personal-data';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'cambiar-contrasena', component: ChangePassword },
+  {
+    path: '',
+    component: HomeLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'datos-personales', component: PersonalData },
+      { path: 'cambiar-contrasena', component: ChangePassword },
+    ],
+  },
 ];
-
