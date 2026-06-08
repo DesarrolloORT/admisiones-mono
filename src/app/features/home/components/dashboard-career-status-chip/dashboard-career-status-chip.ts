6b3b2078b79ba695bd20fbc654dc05c8592916c4
rubino-f <rubino_f@ort.edu.uy>
2026-06-08 15:35:56 -0300

    feat: add dashboard components including status chip, summary, quick actions, and main dashboard page

---
+++ src/app/features/home/components/dashboard-career-status-chip/dashboard-career-status-chip.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-career-status-chip',
+  templateUrl: './dashboard-career-status-chip.html',
+  styleUrl: './dashboard-career-status-chip.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardCareerStatusChip {}
+
--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { HomeLayout } from './layouts/home-layout/home-layout';
 import { ChangePassword } from './pages/change-password/change-password';
+import { Dashboard } from './pages/dashboard/dashboard';
 import { Home } from './pages/home/home';
 import { PersonalData } from './pages/personal-data/personal-data';
 
@@ -11,8 +12,10 @@ export const routes: Routes = [
     component: HomeLayout,
     children: [
       { path: '', component: Home },
+      { path: 'panel-inscripcion', component: Dashboard },
       { path: 'datos-personales', component: PersonalData },
       { path: 'cambiar-contrasena', component: ChangePassword },
     ],
   },
 ];
+
