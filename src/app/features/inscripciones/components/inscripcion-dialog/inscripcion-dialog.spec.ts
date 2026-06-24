01b70a15e67755935929a6d16c572b0982c62687
rubino-f <rubino_f@ort.edu.uy>
2026-06-24 14:39:44 -0300

    refactor: make it a bit more understadable

--- src/app/features/inscripciones/components/inscripcion-dialog/inscripcion-dialog.spec.ts
+++ src/app/features/inscripciones/components/inscripcion-dialog/inscripcion-dialog.spec.ts
@@ -1,24 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars -- placeholder test scaffold */
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { InscripcionDialog } from 'inscripcion-dialog';
+import { TestBed } from '@angular/core/testing';
+
+import { InscripcionDialog } from './inscripcion-dialog';
 
 describe('InscripcionDialog', () => {
-  let component: InscripcionDialog;
-  let fixture: ComponentFixture<InscripcionDialog>;
+  it('creates', () => {
+    TestBed.configureTestingModule({ imports: [InscripcionDialog] }).overrideComponent(
+      InscripcionDialog,
+      { set: { imports: [], template: '' } }
+    );
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [InscripcionDialog],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InscripcionDialog);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should have tests', () => {
-    throw new Error('Test suite not implemented.');
+    expect(TestBed.createComponent(InscripcionDialog).componentInstance).toBeTruthy();
   });
 });
