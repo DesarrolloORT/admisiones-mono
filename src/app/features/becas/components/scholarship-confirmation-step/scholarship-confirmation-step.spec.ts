fb129f22581f9d88f25942142c124d30cb452224
puentesol <puente@ort.edu.uy>
2026-06-29 14:38:21 -0300

    feat(scholarships): implement confirmation step

---
+++ src/app/features/becas/components/scholarship-confirmation-step/scholarship-confirmation-step.spec.ts
@@ -0,0 +1,5 @@
+describe('ScholarshipConfirmationStep', () => {
+  it('should have tests', () => {
+    throw new Error('Test suite not implemented.');
+  });
+});
---
+++ src/app/features/becas/components/scholarship-confirmation-step/scholarship-confirmation-step.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  OrtButtonModule,
+  OrtCardModule,
+  OrtCheckboxModule,
+  OrtIconModule,
+  OrtSnackbarModule,
+} from '@desarrolloort/components';
+
+@Component({
+  selector: 'app-scholarship-confirmation-step',
+  imports: [OrtIconModule, OrtCardModule, OrtButtonModule, OrtSnackbarModule, OrtCheckboxModule],
+  templateUrl: './scholarship-confirmation-step.html',
+  styleUrls: ['./scholarship-confirmation-step.scss', '../../pages/fbr/fbr.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScholarshipConfirmationStep {}
