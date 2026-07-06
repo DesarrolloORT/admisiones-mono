38fce15acd506af610990ce671e8418622d7e85e
puentesol <puente@ort.edu.uy>
2026-07-06 16:52:43 -0300

    feat(scholarships): add routes for other scholarships

--- src/app/features/becas/becas.routes.ts
+++ src/app/features/becas/becas.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { Becas } from './pages/becas/becas';
+import { Fbc } from './pages/fbc/fbc';
 import { Fbr } from './pages/fbr/fbr';
+import { Fcl } from './pages/fcl/fcl';
+import { Fexa } from './pages/fexa/fexa';
 
 export const routes: Routes = [
   { path: '', component: Becas },
   { path: 'fbr', component: Fbr },
+  { path: 'fexa', component: Fexa },
+  { path: 'fcl', component: Fcl },
+  { path: 'fbc', component: Fbc },
 ];
