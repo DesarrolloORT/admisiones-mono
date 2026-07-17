c19f9a1a1413a86862459212b4bbd8afc2f0ecae
puentesol <puente@ort.edu.uy>
2026-07-17 11:47:56 -0300

    style(inscription-reservation-step): change status icon and text color

--- src/app/features/inscriptions/pages/steps/inscription-reservation-step/inscription-reservation-step.ts
+++ src/app/features/inscriptions/pages/steps/inscription-reservation-step/inscription-reservation-step.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { OrtButtonModule, OrtCardModule, OrtIconModule } from '@desarrolloort/components';
+import {
+  OrtButtonModule,
+  OrtCardModule,
+  OrtIconModule,
+  OrtStatusIconModule,
+} from '@desarrolloort/components';
 
 import { InscripcionPaymentFacade } from '../../../facades/inscription-payment';
 
 @Component({
   selector: 'app-inscription-reservation-step',
-  imports: [OrtButtonModule, OrtCardModule, OrtIconModule, RouterLink],
+  imports: [OrtButtonModule, OrtCardModule, OrtIconModule, RouterLink, OrtStatusIconModule],
   templateUrl: './inscription-reservation-step.html',
   styleUrls: ['../../layout.scss', '../inscription-result-step.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
