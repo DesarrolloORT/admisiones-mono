fbeeca165c0750ef44fd47227a30cfd7bd625559
Emanuelle Gabriele González <gabriele@ort.edu.uy>
2026-07-28 10:11:00 -0300

    Update BandejaCorporativa state IDs in InscripcionesConstants
    Updated IdEstadoProcesoInicio and IdEstadoProcesoSolicitud values in the BandejaCorporativa class of InscripcionesConstants.cs to 59643 and 59644, replacing the previous values 57156 and 57157. No other changes were made.

--- WebApiAdmisiones/AppLogic/Inscripciones/Constants/InscripcionesConstants.cs
+++ WebApiAdmisiones/AppLogic/Inscripciones/Constants/InscripcionesConstants.cs
@@ -54,8 +54,8 @@ public static class InscripcionesConstants
     public static class BandejaCorporativa
     {
         public const long IdProceso = 89;
-        public const long IdEstadoProcesoInicio = 57156;
-        public const long IdEstadoProcesoSolicitud = 57157;
+        public const long IdEstadoProcesoInicio = 59643;
+        public const long IdEstadoProcesoSolicitud = 59644;
         public const long IdGrupoResponsable = 48;
         public const string UsuarioSistema = "ADMISIONES";
     }
